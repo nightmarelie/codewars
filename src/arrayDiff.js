1b687e123823777af2ff329d8605d89fb7295081
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-03-22 23:27:42 -0600

    refactor: diff function

--- src/arrayDiff.js
+++ src/arrayDiff.js
@@ -1,8 +1,3 @@
 function arrayDiff(a, b) {
-  return a.reduce((res, cur) => {
-    if (!b.includes(cur)) {
-      res.push(cur);
-    }
-    return res;
-  }, []);
+  return a.filter((i) => !b.includes(i));
 }
