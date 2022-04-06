1c7628401b6b8d6e381c50f5951ee4b16044220a
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-05 21:14:19 -0600

    feat: add invert function

---
+++ src/invert.test.js
@@ -0,0 +1,12 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Invert array values", () => {
+  it("Basic Tests", () => {
+    assert.deepEqual(invert([1, 2, 3, 4, 5]), [-1, -2, -3, -4, -5]);
+    assert.deepEqual(invert([1, -2, 3, -4, 5]), [-1, 2, -3, 4, -5]);
+    assert.deepEqual(invert([]), []);
+    assert.deepEqual(invert([0]), [-0]);
+  });
+});
