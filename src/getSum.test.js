b41cdd7e296a3c45d133e899c68174045fb67699
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-06 22:48:41 -0600

    feat: add get sum

---
+++ src/getSum.test.js
@@ -0,0 +1,10 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(getSum(0, -1), -1);
+    assert.strictEqual(getSum(0, 1), 1);
+  });
+});
