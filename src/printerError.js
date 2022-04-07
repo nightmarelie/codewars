c0f6f1b295aee56d1540b59f5f2468144eed412b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-06 23:56:40 -0600

    feat: add another printer error func

--- src/printerError.js
+++ src/printerError.js
@@ -1,3 +1,13 @@
 function printerError(s) {
   return `${s.replace(/[a-m]/gi, "").length}/${s.length}`;
 }
+
+function printerError1(s) {
+  var count = 0;
+  for (var i = 0; i < s.length; i++) {
+    if (s[i] > "m") {
+      count++;
+    }
+  }
+  return count + "/" + s.length;
+}
