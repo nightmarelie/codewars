db587d335a6a71d8089bc63f012a1ab2f3e45d10
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-07 21:54:56 -0600

    feat: add short version of function

--- src/isSquare.js
+++ src/isSquare.js
@@ -2,3 +2,7 @@ var isSquare = function (n) {
   const isSquare = Number.isInteger(Math.sqrt(n));
   return n < 0 ? false : isSquare;
 };
+
+function isSquare1(n) {
+  return Math.sqrt(n) % 1 === 0;
+}
