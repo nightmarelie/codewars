b178008e980ba05ffe5c513e3faddc8efa6b4b40
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-09 22:45:47 -0600

    feat: remove test case

--- src/getDivisorsCnt.test.js
+++ src/getDivisorsCnt.test.js
@@ -3,6 +3,5 @@ describe("Tests", () => {
     Test.assertEquals(getDivisorsCnt(1), 1);
     Test.assertEquals(getDivisorsCnt(10), 4);
     Test.assertEquals(getDivisorsCnt(11), 2);
-    Test.assertEquals(getDivisorsCnt(54), 8);
   });
 });
