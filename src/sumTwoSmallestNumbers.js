2ed0ad50f2f4fbeeb7b82a5e2f786bcbc98f9a57
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-11 17:38:10 -0600

    feat: add another func

--- src/sumTwoSmallestNumbers.js
+++ src/sumTwoSmallestNumbers.js
@@ -3,3 +3,8 @@ function sumTwoSmallestNumbers(numbers) {
   numbers.splice(numbers.indexOf(min), 1);
   return min + Math.min(...numbers);
 }
+
+function sumTwoSmallestNumbers1(numbers) {
+  var [a, b] = numbers.sort((a, b) => a - b);
+  return a + b;
+}
