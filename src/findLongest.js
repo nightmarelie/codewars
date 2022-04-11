ef4ce08a2dd0788ab497c9f952d06381308f36b3
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-11 17:51:27 -0600

    feat: add more short version of func

--- src/findLongest.js
+++ src/findLongest.js
@@ -2,3 +2,9 @@ function findLongest(array) {
   const maxLength = String(Math.max(...array)).length;
   return array.find((num) => String(num).length === maxLength);
 }
+
+function findLongest(array) {
+  return array.reduce((res, curr) =>
+    String(res).length < String(curr).length ? curr : res
+  );
+}
