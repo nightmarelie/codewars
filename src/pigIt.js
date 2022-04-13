17c48de87e5e2eee1338fe9800b83eaec69144c9
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-13 16:52:05 -0600

    feat: add another func

--- src/pigIt.js
+++ src/pigIt.js
@@ -4,3 +4,7 @@ function pigIt(str) {
     .map((s) => (/\w/gi.test(s) ? `${s.slice(1)}${s[0]}ay` : s))
     .join(" ");
 }
+
+function pigIt(str) {
+  return str.replace(/(\w)(\w*)(\s|$)/g, "$2$1ay$3");
+}
