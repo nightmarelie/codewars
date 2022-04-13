1d74fc607ee09f6bf74585149203ff507cbb83d7
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-13 16:39:16 -0600

    feat: add another two func

--- src/stripsAllText.js
+++ src/stripsAllText.js
@@ -8,3 +8,19 @@ function solution(input, markers) {
     })
     .join("\n");
 }
+
+function solution1(input, markers) {
+  return input
+    .split("\n")
+    .map((line) =>
+      markers.reduce((line, marker) => line.split(marker)[0].trim(), line)
+    )
+    .join("\n");
+}
+
+function solution2(input, markers) {
+  return input.replace(
+    new RegExp("\\s?[" + markers.join("") + "].*(\\n)?", "gi"),
+    "$1"
+  );
+}
