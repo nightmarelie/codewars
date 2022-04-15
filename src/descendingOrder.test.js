a34d887d7f31f7f7df119faac4db286350a7ed1d
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-15 10:59:10 -0600

    feat: add descending order func

---
+++ src/descendingOrder.test.js
@@ -0,0 +1,12 @@
+const { assert } = require("chai");
+
+describe("Basic tests", () => {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(descendingOrder(0), 0);
+    assert.strictEqual(descendingOrder(1), 1);
+    assert.strictEqual(descendingOrder(111), 111);
+    assert.strictEqual(descendingOrder(15), 51);
+    assert.strictEqual(descendingOrder(1021), 2110);
+    assert.strictEqual(descendingOrder(123456789), 987654321);
+  });
+});
