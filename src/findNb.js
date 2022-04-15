592deb99f057efccad5eb0fc4d5ed1f5533c29b1
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-14 20:03:33 -0600

    feat: add find nb func

---
+++ src/findNb.js
@@ -0,0 +1,6 @@
+function findNb(m) {
+  var n = 0;
+  while (m > 0) m -= (++n) ** 3;
+
+  return m ? -1 : n;
+}
---
+++ src/findNb.test.js
@@ -0,0 +1,6 @@
+describe("Basic tests", function () {
+  Test.assertEquals(findNb(4183059834009), 2022);
+  Test.assertEquals(findNb(24723578342962), -1);
+  Test.assertEquals(findNb(135440716410000), 4824);
+  Test.assertEquals(findNb(40539911473216), 3568);
+});
