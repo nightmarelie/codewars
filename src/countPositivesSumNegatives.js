8832aa6ca27e3b92f75a9e626ef57310bfd6881b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-15 16:19:00 -0600

    feat: add another func and refactor previous one

--- src/countPositivesSumNegatives.js
+++ src/countPositivesSumNegatives.js
@@ -1,15 +1,27 @@
 function countPositivesSumNegatives(input) {
-  return (input || []).reduce((acc, n) => {
-    switch (true) {
-      case n > 0:
-        acc[0] = ++acc[0] || 1;
-        acc[1] = acc[1] || 0;
-        break;
-      case n < 0:
-        acc[0] = acc[0] || 0;
-        acc[1] = (acc[1] || 0) + n;
-        break;
-    }
-    return acc;
-  }, []);
+  return input && input.length
+    ? input.reduce(
+        (acc, n) => {
+          switch (true) {
+            case n > 0:
+              acc[0] = ++acc[0];
+              break;
+            case n < 0:
+              acc[1] = acc[1] + n;
+              break;
+          }
+          return acc;
+        },
+        [0, 0]
+      )
+    : [];
+}
+
+function countPositivesSumNegatives1(input) {
+  return input && input.length
+    ? [
+        input.filter((p) => p > 0).length,
+        input.filter((n) => n < 0).reduce((a, b) => a + b, 0),
+      ]
+    : [];
 }
