8de503612b3be19d2a067c329cba691134b35bf2
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-18 11:28:20 -0600

    refactor: fix solution

--- src/DNAStrand.js
+++ src/DNAStrand.js
@@ -25,21 +25,5 @@ function DNAStrand1(dna) {
     C: "G",
     G: "C",
   };
-  return dna
-    .split("")
-    .map((c) => {
-      switch (c) {
-        case "A":
-          return "T";
-        case "T":
-          return "A";
-        case "G":
-          return "C";
-        case "C":
-          return "G";
-        default:
-          return c;
-      }
-    })
-    .join("");
+  return dna.replace(/./g, (c) => pairs[c]);
 }
