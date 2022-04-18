7a951cfc4e6812d76d60e57aea64f4d80a4acca0
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-18 10:39:21 -0600

    feat: add another func

--- src/DNAStrand.js
+++ src/DNAStrand.js
@@ -17,3 +17,29 @@ function DNAStrand(dna) {
     })
     .join("");
 }
+
+function DNAStrand1(dna) {
+  const pairs = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C",
+  };
+  return dna
+    .split("")
+    .map((c) => {
+      switch (c) {
+        case "A":
+          return "T";
+        case "T":
+          return "A";
+        case "G":
+          return "C";
+        case "C":
+          return "G";
+        default:
+          return c;
+      }
+    })
+    .join("");
+}
