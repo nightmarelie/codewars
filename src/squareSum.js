000d5be4a1e1050be400042f362e5290bdc3c2bd
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-19 21:26:50 -0600

    feat: add another function

--- src/squareSum.js
+++ src/squareSum.js
@@ -1,3 +1,7 @@
 function squareSum(numbers) {
   return numbers.map((n) => n ** 2).reduce((acc, i) => acc + i, 0);
 }
+
+function squareSum(numbers) {
+  return numbers.reduce((sum, num) => sum + num * num, 0);
+}
