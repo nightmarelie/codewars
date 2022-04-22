911460048ec75b31f098b8be8b50eb5c24f193d2
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-21 21:24:16 -0600

    feat: add array of sheep func

--- src/countSheeps.js
+++ src/countSheeps.js
@@ -1,3 +1,7 @@
 function countSheeps(arrayOfSheep) {
   return arrayOfSheep.reduce((count, current) => count + Number(!!current), 0);
 }
+
+function countSheeps(arrayOfSheeps) {
+  return arrayOfSheeps.filter(Boolean).length;
+}
