f4ffa37d0d27920c60f9fab5f77ad8bc4c86e0d9
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-22 22:23:00 -0600

    feat: add another function

--- src/countSmileys.js
+++ src/countSmileys.js
@@ -1,3 +1,7 @@
 function countSmileys(arr) {
   return arr.filter((smile) => !!smile.match(/[\:;][-~]?[\)D]/gi)).length;
 }
+
+function countSmileys1(arr) {
+  return arr.filter((smile) => /[\:;][-~]?[\)D]/.test(smile)).length;
+}
