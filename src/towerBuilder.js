4984bd02902feeceec3e2ec3ebffd3722e176d90
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-23 22:32:29 -0600

    feat: add another func

--- src/towerBuilder.js
+++ src/towerBuilder.js
@@ -10,3 +10,10 @@ function towerBuilder(nFloors) {
         .padEnd(maxLength);
     });
 }
+
+function towerBuilder(n) {
+  return Array.from({ length: n }, function (v, k) {
+    const spaces = " ".repeat(n - k - 1);
+    return spaces + "*".repeat(k + k + 1) + spaces;
+  });
+}
