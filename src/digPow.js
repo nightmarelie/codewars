6863394bdbe4542b4f467fa68cf32851b1fde38e
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-24 11:58:48 -0600

    feat: add another func

--- src/digPow.js
+++ src/digPow.js
@@ -8,3 +8,10 @@ function digPow(n, p) {
 
   return n * result === num ? result : -1;
 }
+
+function digPow(n, p) {
+  var x = String(n)
+    .split("")
+    .reduce((s, d, i) => s + Math.pow(d, p + i), 0);
+  return x % n ? -1 : x / n;
+}
