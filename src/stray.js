621fe1c80b0af93aac865c9d04ae0b88efc8ce61
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-24 12:29:28 -0600

    feat: add another func

--- src/stray.js
+++ src/stray.js
@@ -10,3 +10,11 @@ function stray(numbers) {
     }
   }
 }
+
+function stray(numbers) {
+  for (var i in numbers) {
+    if (numbers.indexOf(numbers[i]) === numbers.lastIndexOf(numbers[i])) {
+      return numbers[i];
+    }
+  }
+}
