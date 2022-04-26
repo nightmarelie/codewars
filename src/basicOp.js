0627b11dd01e1b39bb47b1cd2549fc22a81acd9a
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-25 21:52:12 -0600

    feat: add another func

--- src/basicOp.js
+++ src/basicOp.js
@@ -1,3 +1,13 @@
 function basicOp(operation, value1, value2) {
   return eval(`${value1} ${operation} ${value2}`);
 }
+
+function basicOp(operation, value1, value2) {
+  var cases = {
+    "+": value1 + value2,
+    "-": value1 - value2,
+    "*": value1 * value2,
+    "/": value1 / value2,
+  };
+  return cases[operation];
+}
