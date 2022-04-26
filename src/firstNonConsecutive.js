6db6105ea932d333e8dc8f1585c3e124254e73f3
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-25 22:34:13 -0600

    feat: add another func

--- src/firstNonConsecutive.js
+++ src/firstNonConsecutive.js
@@ -6,3 +6,9 @@ function firstNonConsecutive(arr) {
   }
   return null;
 }
+
+function firstNonConsecutive(arr) {
+  let result = arr.find((val, index) => val !== index + arr[0]);
+
+  return Number.isInteger(result) ? result : null;
+}
