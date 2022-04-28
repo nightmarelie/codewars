0c6a5dace3b120061e230038aedd442cbe6496ac
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-28 10:52:46 -0600

    feat: add another func

--- src/removeSmallest.js
+++ src/removeSmallest.js
@@ -15,3 +15,8 @@ function removeSmallest(numbers) {
   let indexOfMin = numbers.indexOf(Math.min(...numbers));
   return [...numbers.slice(0, indexOfMin), ...numbers.slice(indexOfMin + 1)];
 }
+
+function removeSmallest(numbers) {
+  const indexOfMin = numbers.indexOf(Math.min(...numbers));
+  return numbers.filter((_, idx) => idx !== indexOfMin);
+}
