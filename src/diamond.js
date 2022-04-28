58c9cda5ef2cf63202fcfcd0a995a1c61024f495
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-28 09:50:44 -0600

    feat: add another func

--- src/diamond.js
+++ src/diamond.js
@@ -15,3 +15,28 @@ function diamond(n) {
 
   return [...topStars, centerStars, ...bottonStars].join("\n") + "\n";
 }
+
+function diamond(n) {
+  if (n <= 0 || n % 2 === 0) return null;
+  str = "";
+  for (let i = 0; i < n; i++) {
+    let len = Math.abs((n - 2 * i - 1) / 2);
+    str += " ".repeat(len);
+    str += "*".repeat(n - 2 * len);
+    str += "\n";
+  }
+  return str;
+}
+
+function diamond(n) {
+  if (n <= 0 || n % 2 === 0) return null;
+
+  return Array(n)
+    .fill("*")
+    .map((star, idx) => {
+      const length = Math.abs((n - 2 * idx - 1) / 2);
+      const starLen = n - 2 * length;
+      return star.repeat(starLen).padStart(length + starLen) + "\n";
+    })
+    .join("");
+}
