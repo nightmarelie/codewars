597b72061db81e4424e800454ac72305b58e5cc8
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-27 19:49:34 -0600

    feat: add other angle func

---
+++ src/otherAngle.test.js
@@ -0,0 +1,12 @@
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold = 0;
+
+describe("Tests", () => {
+  it("test", () => {
+    assert.strictEqual(otherAngle(30, 60), 90);
+    assert.strictEqual(otherAngle(60, 60), 60);
+    assert.strictEqual(otherAngle(43, 78), 59);
+    assert.strictEqual(otherAngle(10, 20), 150);
+  });
+});
