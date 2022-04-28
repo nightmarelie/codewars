3d2bf312521057a7b1d8b070edfaa22a8e781c3a
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-27 20:12:34 -0600

    feat: add another func

--- src/titleCase.js
+++ src/titleCase.js
@@ -17,3 +17,22 @@ function titleCase(title, minorWords = "") {
     })
     .join(" ");
 }
+
+function titleCase(title, minorWords) {
+  var minorWords =
+    typeof minorWords !== "undefined"
+      ? minorWords.toLowerCase().split(" ")
+      : [];
+  return title
+    .toLowerCase()
+    .split(" ")
+    .map(function (v, i) {
+      if (v != "" && (minorWords.indexOf(v) === -1 || i == 0)) {
+        v = v.split("");
+        v[0] = v[0].toUpperCase();
+        v = v.join("");
+      }
+      return v;
+    })
+    .join(" ");
+}
