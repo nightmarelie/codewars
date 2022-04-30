bddb9ac6cec9ec619b43441e1d712048bba9a260
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-04-29 18:53:33 -0600

    feat: add greet func

---
+++ src/greet.test.js
@@ -0,0 +1,19 @@
+describe("Tests", () => {
+  it("test", () => {
+    Test.assertEquals(
+      greet("english"),
+      "Welcome",
+      "Your function should have returned 'Welcome'. Try again."
+    );
+    Test.assertEquals(
+      greet("dutch"),
+      "Welkom",
+      "Your function should have returned 'Welkom'. Try again."
+    );
+    Test.assertEquals(
+      greet("IP_ADDRESS_INVALID"),
+      "Welcome",
+      "Your function should have returned 'Welcome'. Try again."
+    );
+  });
+});
