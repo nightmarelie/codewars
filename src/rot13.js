3db555601d43dcc0644d02b53a06f5ec9fb16ff0
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-09 21:42:38 -0600

    feat: add another function

--- src/rot13.js
+++ src/rot13.js
@@ -5,3 +5,10 @@ function rot13(message) {
     )
   );
 }
+
+const rot13 = (str) =>
+  str.replace(/[a-z]/gi, (letter) =>
+    String.fromCharCode(
+      letter.charCodeAt(0) + (letter.toLowerCase() <= "m" ? 13 : -13)
+    )
+  );
