7b0799bb2a6a1accbc5686a533a2bab01461e93b
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-09 21:48:53 -0600

    feat: add simple solution

--- src/twoSum.js
+++ src/twoSum.js
@@ -14,3 +14,11 @@ function twoSum(numbers, target) {
 
   return res;
 }
+
+function twoSum(numbers, target) {
+  for (var i = 0; i < numbers.length - 1; i++) {
+    for (var j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) return [i, j];
+    }
+  }
+}
