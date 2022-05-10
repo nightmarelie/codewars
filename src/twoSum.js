5b828f6707ddc9fc2bebcaf7737cbd880e5482bb
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-09 21:51:31 -0600

    feat: solution using hash

--- src/twoSum.js
+++ src/twoSum.js
@@ -22,3 +22,16 @@ function twoSum(numbers, target) {
     }
   }
 }
+
+function twoSum(numbers, target) {
+  var tmp,
+    hash = {};
+  numbers.forEach(function (a, i) {
+    hash[a] = i;
+  });
+
+  for (var i = 0; i < numbers.length; i++) {
+    tmp = target - numbers[i];
+    if (typeof hash[tmp] !== "undefined") return [i, hash[tmp]];
+  }
+}
