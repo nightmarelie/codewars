7f835bb136d6e1fcadc049934c19f08bc976bcdb
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-05-11 22:59:16 -0600

    feat: add another func

--- src/duplicateEncode.js
+++ src/duplicateEncode.js
@@ -8,3 +8,13 @@ function duplicateEncode(word) {
     )
     .join("");
 }
+
+function duplicateEncode(word) {
+  return word
+    .toLowerCase()
+    .split("")
+    .map(function (a, i, w) {
+      return w.indexOf(a) == w.lastIndexOf(a) ? "(" : ")";
+    })
+    .join("");
+}
